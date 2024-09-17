4f2fe4a1b171d383574290c9edd0955e43138481
Fallet Steve <steve.fallet@divtec.ch>
2024-09-17 16:48:16 +0200

    code au 17.9.24

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
-/**
+    /**
  * Intro à vue.js
  * @author Steve Fallet
  * @date 2024-09-17
  */
+const { createApp, ref } = Vue;
 
-console.log('Intro à vue.js');
+const app = createApp({
+    setup() {
+        // Datas
+        const message = "Bienvenue dans PokeCount!";
+        const compteur = ref(0);
+        const capturesTab = ref([]);
+        // Fonctions
+        function capturer() {
+            compteur.value += 1;
+        }
+        function sauver() {
+            if (compteur.value > 0) {
+                capturesTab.value.push(compteur.value);
+                compteur.value = 0;
+            }
+        }
+        // Retourne les objets dans le HTML
+        return {
+            message,
+            compteur,
+            capturer,
+            capturesTab,
+            sauver
+        };
+    }
+}).mount("#poke-app");
